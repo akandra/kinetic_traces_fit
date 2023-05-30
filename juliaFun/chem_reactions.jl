396623807088875fe00fa4d4a9b040f27bf25264
akandra <akandra1@gmail.com>
2023-05-30 13:50:20 +0200

    follow up

--- juliaFun/chem_reactions.jl
+++ juliaFun/chem_reactions.jl
@@ -2,8 +2,8 @@ using Catalyst, DifferentialEquations, Plots
 using Latexify
 
 reqs = @reaction_network begin
-    k₁, H₂ +  O --> OH + H
-    kᵦ, OH +  H --> H₂ + O
+    (k₁,kᵦ), H₂ +  O <--> OH + H
+#    kᵦ, OH +  H --> H₂ + O
     k₂,  H + OH --> H₂O
     k₃, OH + OH --> H₂O
     k₄,  H +  H --> H₂
@@ -12,7 +12,6 @@ reqs = @reaction_network begin
 
 latexify(reqs, env=:chemical) |> render
 latexify(reqs,form=:ode) |> render
-
 
   p = (:k₁ => 1.0, :kᵦ => 1e-3,:k₂ => 1.0,:k₃ => 10.0,:k₄ => 1.0,:k₅ => 1.0)
   tspan = (0.,10.)
